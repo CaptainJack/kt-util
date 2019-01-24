713493af3b9fcd8ba9065f2c1422a352e8e0cd36
Alexey Chuprin <a.chuprin@capjack.ru>
2019-01-24 10:44:46 +0300

    Kotlin 1.3.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation.Companion.MAIN_COMPILATION_NAME
 
 plugins {
-	kotlin("multiplatform") version "1.3.11"
+	kotlin("multiplatform") version "1.3.20"
 	id("nebula.release") version "9.2.0"
 	id("ru.capjack.capjack-bintray") version "0.14.1"
 }
@@ -13,48 +12,44 @@ repositories {
 	jcenter()
 }
 
-afterEvaluate {
-	// https://youtrack.jetbrains.com/issue/KT-29058
-	publishing.publications.forEach { (it as MavenPublication).groupId = group.toString() }
-}
-
 kotlin {
-	targets {
-		add(presets["jvm"].createTarget("jvm").apply {
-			compilations.all {
-				tasks.getByName<KotlinJvmCompile>(compileKotlinTaskName).kotlinOptions.jvmTarget = "1.8"
+	
+	jvm {
+		compilations.all {
+			kotlinOptions {
+				jvmTarget = "1.8"
 			}
-		})
-		
-		add(presets["js"].createTarget("js").apply {
-			compilations.all {
-				tasks.getByName<KotlinJsCompile>(compileKotlinTaskName).kotlinOptions {
-					sourceMap = true
-					sourceMapEmbedSources = "always"
-					moduleKind = "umd"
-				}
+		}
+	}
+	
+	js {
+		compilations.all {
+			kotlinOptions {
+				sourceMap = true
+				sourceMapEmbedSources = "always"
+				moduleKind = "umd"
 			}
-		})
+		}
 	}
 	
 	sourceSets {
 		commonMain {
 			dependencies {
-				implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+				implementation(kotlin("stdlib-common"))
 			}
 		}
 		
-		named("jvmMain") {
+		jvm().compilations[MAIN_COMPILATION_NAME].defaultSourceSet {
 			dependencies {
-				implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+				implementation(kotlin("stdlib-jdk8"))
 			}
 		}
 		
-		named("jsMain") {
+		js().compilations[MAIN_COMPILATION_NAME].defaultSourceSet {
 			dependencies {
-				implementation("org.jetbrains.kotlin:kotlin-stdlib-js")
+				implementation(kotlin("stdlib-js"))
 			}
 		}
 	}
+	
 }
-
