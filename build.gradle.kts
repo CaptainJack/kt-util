9bd4ba9de3901cfc1bb4e6ce1f398f43330680cc
Alexey Chuprin <a.chuprin@capjack.ru>
2019-06-26 10:44:56 +0300

    Kotlin 1.3.40

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-import org.jetbrains.kotlin.cli.common.arguments.K2JsArgumentConstants
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
-
 plugins {
-	kotlin("multiplatform") version "1.3.31"
-	id("nebula.release") version "10.0.1"
+	kotlin("multiplatform") version "1.3.40"
+	id("nebula.release") version "10.1.2"
 	id("ru.capjack.bintray") version "0.17.0"
 }
 
@@ -15,35 +12,20 @@ repositories {
 
 kotlin {
 	sourceSets {
-		commonMain {
-			dependencies {
-				implementation(kotlin("stdlib-common"))
-			}
+		commonMain.get().dependencies {
+			implementation(kotlin("stdlib-common"))
 		}
 	}
 	
 	jvm().compilations {
-		all {
-			kotlinOptions.jvmTarget = "1.8"
-		}
-		
-		get(KotlinCompilation.MAIN_COMPILATION_NAME).defaultSourceSet {
-			dependencies {
-				implementation(kotlin("stdlib-jdk8"))
-			}
+		get("main").defaultSourceSet.dependencies {
+			implementation(kotlin("stdlib-jdk8"))
 		}
 	}
 	
 	js().compilations {
-		all {
-			kotlinOptions.moduleKind = K2JsArgumentConstants.MODULE_UMD
-		}
-		
-		get(KotlinCompilation.MAIN_COMPILATION_NAME).defaultSourceSet {
-			dependencies {
-				implementation(kotlin("stdlib-js"))
-			}
+		get("main").defaultSourceSet.dependencies {
+			implementation(kotlin("stdlib-js"))
 		}
 	}
-	
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "tool-lang"
+
+enableFeaturePreview("GRADLE_METADATA")
