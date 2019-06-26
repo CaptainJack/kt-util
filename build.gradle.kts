5380e7a1ffa9ae4cc058308c3be42824062aa833
Alexey Chuprin <a.chuprin@capjack.ru>
2019-06-26 11:00:35 +0300

    ru.capjack.bintray 0.18.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
 plugins {
 	kotlin("multiplatform") version "1.3.40"
 	id("nebula.release") version "10.1.2"
-	id("ru.capjack.bintray") version "0.17.0"
+	id("ru.capjack.bintray") version "0.18.0"
 }
 
 group = "ru.capjack.tool"
@@ -27,5 +29,15 @@ kotlin {
 		get("main").defaultSourceSet.dependencies {
 			implementation(kotlin("stdlib-js"))
 		}
+	}
+}
+
+afterEvaluate {
+	rootProject.extensions.getByType<PublishingExtension>().publications.forEach {
+		println(it.name)
+	}
+	
+	project.extensions.findByType<KotlinMultiplatformExtension>()!!.targets.forEach {
+		println(it.name + " " + it.publishable)
 	}
 }
