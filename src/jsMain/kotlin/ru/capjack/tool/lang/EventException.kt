138663642013b38581782cb5909fe68de67636c0
Alexey Chuprin <a.chuprin@capjack.ru>
2019-08-01 14:57:54 +0300

    Added js EventException

---
+++ src/jsMain/kotlin/ru/capjack/tool/lang/EventException.kt
@@ -0,0 +1,8 @@
+package ru.capjack.tool.lang
+
+import org.w3c.dom.events.Event
+
+open class EventException(event: Event) : RuntimeException(
+	"${event::class.js.name}(${event.type})".let { m -> (event.asDynamic().message as String?)?.let { "$m: $it" } ?: m },
+	event.asDynamic().error.unsafeCast<Throwable?>()
+)
