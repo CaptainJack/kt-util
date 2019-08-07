0056f5d5a3cb0e32ec7a15ff0335f15815ebd218
Alexey Chuprin <a.chuprin@capjack.ru>
2019-08-07 15:06:28 +0300

    JVM thread functions use values of milliseconds as Int instead Long

--- src/jvmMain/kotlin/ru/capjack/tool/lang/_thread.kt
+++ src/jvmMain/kotlin/ru/capjack/tool/lang/_thread.kt
@@ -1,20 +1,20 @@
 package ru.capjack.tool.lang
 
-import ru.capjack.tool.lang.lefIf
-
-inline fun waitIf(maxTimeoutMillis: Long, checkTimeoutMillis: Long = 10, check: () -> Boolean): Boolean {
+inline fun waitIf(maxTimeoutMillis: Int, checkTimeoutMillis: Int = 10, check: () -> Boolean): Boolean {
 	return lefIf(check()) {
 		waitIfImmediately(maxTimeoutMillis, checkTimeoutMillis, check)
 	}
 }
 
-inline fun waitIfImmediately(maxTimeoutMillis: Long, checkTimeoutMillis: Long = 10, check: () -> Boolean): Boolean {
+inline fun waitIfImmediately(maxTimeoutMillis: Int, checkTimeoutMillis: Int = 10, check: () -> Boolean): Boolean {
 	var b: Boolean
 	val time = System.currentTimeMillis()
+	val checkTimeoutMillisLong = checkTimeoutMillis.toLong()
+	val maxTimeoutMillisLong = maxTimeoutMillis.toLong()
 	do {
-		Thread.sleep(checkTimeoutMillis)
+		Thread.sleep(checkTimeoutMillisLong)
 		b = check()
 	}
-	while (b && System.currentTimeMillis() - time < maxTimeoutMillis)
+	while (b && System.currentTimeMillis() - time < maxTimeoutMillisLong)
 	return b
 }
