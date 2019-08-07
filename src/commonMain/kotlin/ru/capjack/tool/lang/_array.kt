bdee92466a8f4edefa142578d6f340b5828cf169
Alexey Chuprin <a.chuprin@capjack.ru>
2019-08-08 00:36:38 +0300

    Added EMPTY_BYTE_ARRAY

--- src/commonMain/kotlin/ru/capjack/tool/lang/_array.kt
+++ src/commonMain/kotlin/ru/capjack/tool/lang/_array.kt
@@ -1,5 +1,6 @@
 package ru.capjack.tool.lang
 
+val EMPTY_BYTE_ARRAY = ByteArray(0)
 val EMPTY_INT_ARRAY = IntArray(0)
 
 expect fun <T> Array<T>.fill(element: T, fromIndex: Int = 0, toIndex: Int = size)
