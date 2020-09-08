e04f663c51472deb2d22c9db2b0ada8d10bcb6c5
Alexey Chuprin <a.chuprin@capjack.ru>
2020-09-08 18:27:35 +0300

    gradle 6.6.1 kotlin 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("multiplatform") version "1.3.72"
+	kotlin("multiplatform") version "1.4.0"
 	id("nebula.release") version "15.1.0"
 	id("ru.capjack.bintray") version "1.0.0"
 }
@@ -15,30 +15,20 @@ kotlin {
 	jvm {
 		compilations.all { kotlinOptions.jvmTarget = "1.8" }
 	}
-	js {
-		compilations.all { kotlinOptions.sourceMap = false }
+	js(IR) {
 		browser()
 	}
 	
 	sourceSets {
-		get("commonMain").dependencies {
-			implementation(kotlin("stdlib-common"))
-		}
 		get("commonTest").dependencies {
 			implementation(kotlin("test-common"))
 			implementation(kotlin("test-annotations-common"))
 		}
 		
-		get("jvmMain").dependencies {
-			implementation(kotlin("stdlib-jdk8"))
-		}
 		get("jvmTest").dependencies {
 			implementation(kotlin("test-junit"))
 		}
 		
-		get("jsMain").dependencies {
-			implementation(kotlin("stdlib-js"))
-		}
 		get("jsTest").dependencies {
 			implementation(kotlin("test-js"))
 		}
