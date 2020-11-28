87e2d2eb2390e7d3523a45e237ee18138c1e1803
Alexey Chuprin <a.chuprin@capjack.ru>
2020-11-28 23:45:49 +0300

    String utils

---
+++ src/commonMain/kotlin/ru/capjack/tool/lang/_string.kt
@@ -0,0 +1,9 @@
+package ru.capjack.tool.lang
+
+fun String.ensureEnd(suffix: String): String {
+	return if (endsWith(suffix)) return this else this + suffix
+}
+
+fun String.ensureEnd(suffix: Char): String {
+	return if (endsWith(suffix)) return this else this + suffix
+}
